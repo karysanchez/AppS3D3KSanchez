779125750f6b77d123a1947f1822b98d12bc86d5
Karina Sanchez <karoty_25@hotmail.com>
2021-10-24 16:37:56 -0500

    Validación ingreso notas

--- AppS3D3KSanchez/MainPage.xaml.cs
+++ AppS3D3KSanchez/MainPage.xaml.cs
@@ -14,5 +14,56 @@ namespace AppS3D3KSanchez
         {
             InitializeComponent();
         }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Variables de almacenamiento
+                double notaUno = Convert.ToDouble(txtNotaUno.Text);
+                double examenUno = Convert.ToDouble(txtExamenUno.Text);
+
+                double notaDos = Convert.ToDouble(txtNotaDos.Text);
+                double examenDos = Convert.ToDouble(txtExamenDos.Text);
+
+                //Operaciones a calcular
+                if (notaUno < 0.1 || notaUno > 10 || notaDos < 0.1 || notaDos > 10 || examenUno < 0.1 || examenUno > 10 || examenDos < 0.1 || examenDos > 10)
+                {
+                    DisplayAlert("IMPORTANTE", "Los valores de las notas NO deben ser menor a 0.1, NI mayor a 10", "OK");
+                }
+                else
+                {
+                    double notaParcialUno = (notaUno * 0.3) + (examenUno * 0.2);
+                    double notaParcialDos = (notaDos * 0.3) + (examenDos * 0.2);
+
+                    double notaFinal = notaParcialUno + notaParcialDos;
+
+                    //Visualizar resultado operaciones
+                    txtParcialUno.Text = notaParcialUno.ToString();
+                    txtParcialDos.Text = notaParcialDos.ToString();
+                    txtNotaFinal.Text = notaFinal.ToString();
+
+                    //Condiciones de estado
+
+                    if (notaFinal >= 7)
+                    {
+                        txtEstado.Text = "APROBADO";
+                    }
+                    else if (notaFinal >= 5 && notaFinal <= 6.9)
+                    {
+                        txtEstado.Text = "COMPLEMENTARIO";
+                    }
+                    else
+                    {
+                        txtEstado.Text = "REPROBADO";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de alerta", ex.Message, "ok");
+            }
+        }
     }
 }
